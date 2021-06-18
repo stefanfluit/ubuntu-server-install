0dfcf6caa24dea8cf3fb5fb2a8136fd4ca1afeac
Stefan Fluit <stefan@fluit-online.nl>
2021-06-18 20:49:08 +0200

    Add UFW, docker permissions, and other changes

--- install.sh
+++ install.sh
@@ -19,26 +19,25 @@ add_user() {
     touch "/home/${user}/.ssh/authorized_keys"
     chmod 600 "/home/${user}/.ssh/authorized_keys"
     chown -R "${user}:${user}" "/home/${user}/.ssh/"
-    echo "${user} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    ssh-keygen -t ed25519 -f "/home/${user}/.ssh/id_ed25519_${user}" -C "${user}" -q -N ""
 }
 
 configure_zsh() {
     local user
     user="${1}"
     sudo -u "${user}" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    sudo -u "${user}" wget -O /home/robin/.zshrc https://raw.githubusercontent.com/stefanfluit/default_files/master/oh_my_zsh/.zshrc-pnd
+    sudo -u "${user}" wget -O "/home/${user}/.zshrc" https://raw.githubusercontent.com/stefanfluit/default_files/master/oh_my_zsh/.zshrc-pnd
     sed -i "s/empty-user/${user}/g" "/home/${user}/.zshrc"
     chsh -s $(which zsh) "${user}"
 }
 
 declare -a users=(
     "fluit"
-    "robin"
+    #"robin"
 )
 
 declare -a keys=(
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC7N8v/BSL254L8Ph2CchKMGRNW0wpfh+6d1dR4o5plC stefan.fluit@fedora.robinradar.systems"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA/4jgVyo1ZNtfnF5DJh0k0wQOmg8oLowhHaFU/Vw7n1 stefan.fluit@robinradar.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGUqQRxnAmFzG2XhkMcfItx+S8yZmAXpDQxq3oJfGm3q stefan.fluit@nextcloud.fluit@fedora.fluit-online.nl"
 )
 
 declare -a programs=(
@@ -53,10 +52,22 @@ declare -a programs=(
   "gnupg-agent"
   "vim"
   "mtr"
+  "open-vm-tools"
+  "net-tools"
 )
+
+# Update server
+apt-get update && apt-get upgrade -y
+
+for program in "${programs[@]}"; do
+    apt-get -y install "${program}"
+done
 
 for user in "${users[@]}"; do
     add_user "${user}"
+done
+
+for user in "${users[@]}"; do
     configure_zsh "${user}"
 done
 
@@ -64,10 +75,6 @@ for key in "${keys[@]}"; do
     for user in "${users[@]}"; do
         printf "%s\n" "${key}" >> "/home/${user}/.ssh/authorized_keys"
     done
-done
-
-for program in "${programs[@]}"; do
-    apt-get -y install "${program}"
 done
 
 # Commands
@@ -83,6 +90,11 @@ curl https://api.github.com/repos/docker/compose/releases/latest | grep browser_
 cp docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# Docker permissions
+for user in "${users[@]}"; do
+    usermod -aG docker "${user}"
+done
+
 # Download latest Prometheus Node Exporter
 curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
 tar xvf  $(find $(pwd) -name 'node_exporter*.tar.gz')
@@ -94,3 +106,19 @@ systemctl daemon-reload
 systemctl enable node_exporter --now
 rm -rf $(find $(pwd) -name 'node_exporter*.tar.gz')
 rm -rf $(find $(pwd) -name 'node_exporter*' -type d)
+
+# Configure UFW:
+# Deny all incoming traffic
+ufw default deny incoming
+
+# Accept all outgoing traffic
+ufw default allow outgoing
+
+# Allow HTTP and HTTPS traffic for web Traefik
+ufw allow http && ufw allow https
+
+# SSH
+ufw allow from "217.63.222.64/32" to any port 22
+
+# Enable the firewall
+ufw enable
